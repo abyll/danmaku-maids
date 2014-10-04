90ccf7e25774812a49ac3509cec20e05825aa7f3
Abyll <abyll@neejean.org>
2014-10-04 12:31:02 -0600

    Created player, level, and player-controlling scripts.Initial folder structure

---
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = new Vector3(Input.GetAxis("HorizontalAim"), Input.GetAxis("VerticalAim"), 0.0f);
+		transform.GetChild(0).localPosition = pos;
+		Debug.Log (pos);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Input.GetAxis("Horizontal")*speed, Input.GetAxis("Vertical")*speed, 0);
+		if(Input.GetAxis("Horizontal") < 0)
+			transform.localScale.Set(-1,1,1);
+		else
+			transform.localScale.Set(1,1,1);
+	}
+}
