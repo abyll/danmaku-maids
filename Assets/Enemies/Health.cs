c526077fa65aff74fd781221639d31e055a91434
barton.jamesr <barton.jamesr@gmail.com>
2014-10-05 21:19:01 -0600

    Attempted to refine hitboxes on enemies

---
+++ Assets/Enemies/Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	public int health = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Hit(){
+		health--;
+		if(health == 0){
+			Destroy (gameObject);
+		}
+	}
+}
